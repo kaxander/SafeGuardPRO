bc2c3d4d2d41853cd414fc8f026eabbfc2497366
Gabriel Alexandre <gabriel.alexandre4@portalsesisp.org.br>
2024-04-01 11:10:09 -0300

    Ta tudo certo agora.

--- app/src/main/java/com/fourprograms/safeguardpro/view/RelatorioEpiFragment.kt
+++ app/src/main/java/com/fourprograms/safeguardpro/view/RelatorioEpiFragment.kt
@@ -1,10 +1,11 @@
-package com.fourprograms.safeguardpro
+package com.fourprograms.safeguardpro.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.fourprograms.safeguardpro.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [homesup.newInstance] factory method to
+ * Use the [RelatorioEpiFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class homesup : Fragment() {
+class RelatorioEpiFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class homesup : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.`pagsupervisor.xml`, container, false)
+        return inflater.inflate(R.layout.fragment_relatorio_epi, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class homesup : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment homesup.
+         * @return A new instance of fragment RelatorioEpiFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            homesup().apply {
+            RelatorioEpiFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/fourprograms/safeguardpro/view/RelatorioFuncionarioFragment.kt
+++ app/src/main/java/com/fourprograms/safeguardpro/view/RelatorioFuncionarioFragment.kt
@@ -1,10 +1,11 @@
-package com.fourprograms.safeguardpro
+package com.fourprograms.safeguardpro.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.fourprograms.safeguardpro.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -13,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [pagina3.newInstance] factory method to
+ * Use the [RelatorioFuncionarioFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class pagina3 : Fragment() {
+class RelatorioFuncionarioFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +35,7 @@ class pagina3 : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pagina3, container, false)
+        return inflater.inflate(R.layout.fragment_relatorio_funcionario, container, false)
     }
 
     companion object {
@@ -44,12 +45,12 @@ class pagina3 : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment pagina3.
+         * @return A new instance of fragment RelatorioFuncionarioFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            pagina3().apply {
+            RelatorioFuncionarioFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
